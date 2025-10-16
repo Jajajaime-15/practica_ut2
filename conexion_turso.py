d08d56955ceeb2260174094415b79c8ee354f3cc
DAM2B-24 <jaime.ruiz8@educa.madrid.org>
2025-10-16 19:59:32 +0200

    Commit primera clase

---
+++ conexion_turso.py
@@ -0,0 +1,23 @@
+import envyte # type: ignore
+import libsql # type: ignore
+
+db_url = envyte.get("DB_URL")
+auth_token = envyte.get("API_TOKEN")
+
+conn = libsql.connect("practicaut2",sync_url = db_url, auth_token = auth_token)
+
+cursor = conn.cursor()
+
+cursor.execute('''
+                CREATE TABLE IF NOT EXISTS prueba (
+                    col1 INTEGER PRIMARY KEY,
+                    col2 TEXT NOT NULL,
+                    col3 TEXT NOT NULL
+                )
+                ''')
+
+cursor.execute('''INSERT INTO prueba (col2, col3) VALUES ('valorPrueba1','valorPrueba2')''')
+cursor.commit()
+cursor.sync()
+
+conn.close()
