824f0772d3cae25ff1608c9a0b461c5c1e2647f4
Jaime Ruiz <jajajaime1503@gmail.com>
2025-10-24 21:55:18 +0200

    Inserts y Drops de Ingesta

--- ingesta.py
+++ ingesta.py
@@ -8,6 +8,11 @@ conn = libsql.connect("practicaut2",sync_url = db_url, auth_token = auth_token)
 
 cursor = conn.cursor()
 
+cursor.execute('''DROP TABLE IF EXISTS editorial''')
+cursor.execute('''DROP TABLE IF EXISTS genero''')
+cursor.execute('''DROP TABLE IF EXISTS autor''')
+cursor.execute('''DROP TABLE IF EXISTS libros''')
+
 cursor.execute('''
                 CREATE TABLE IF NOT EXISTS editorial (
                     id_editorial INTEGER PRIMARY KEY,
@@ -42,20 +47,20 @@ cursor.execute('''
 
 
 cursor.execute('''
-                INSERT INTO editorial VALUES ('Nova'), ('Egregius'), ('Dykinson'), ('Hidra'), ('RBA')
+                INSERT INTO editorial (nombre) VALUES ('Nova'), ('Egregius'), ('Dykinson'), ('Hidra'), ('RBA')
                 ''')
 cursor.execute('''
-                INSERT INTO autor (nombre, apellido, edad) 
-                VALUES ('Brandon','Sanderson','50'), ('Jaime','Ruiz','23'), ('Pierce','Brown','37'), 
-                    ('Rebecca','Kuang','30'), ('Vicente','Garrido','67')
+                INSERT INTO autor (nombre_completo, edad) 
+                VALUES ('Brandon Sanderson','50'), ('Jaime Ruiz','23'), ('Pierce Brown','37'), 
+                    ('Rebecca Kuang','30'), ('Vicente Garrido','67')
                 ''')
 cursor.execute('''
-                INSERT INTO genero VALUES ('Fantasia'), ('Ciencia Ficcion'), ('Policiaca'), ('Ensayo'), ('Horror')
+                INSERT INTO genero(nombre) VALUES ('Fantasia'), ('Ciencia Ficcion'), ('Policiaca'), ('Ensayo'), ('Horror')
                 ''')
 cursor.execute('''
                 INSERT INTO libros (titulo, id_autor, id_genero, id_editorial, num_pags) 
                 VALUES ('El Camino de los Reyes',1,1,1,1100), ('Analisis de la discriminacion hacia la mujer en los videojuegos',2,4,3,25), 
-                (Juramentada',1,1,1,1300), ('Nacidos de la Bruma',1,1,1,650), ('La mujer en los videojuegos: desigualdad y discriminación',2,4,3,6)
+                ('Juramentada',1,1,1,1300), ('Nacidos de la Bruma',1,1,1,650), ('La mujer en los videojuegos: desigualdad y discriminación',2,4,3,6)
                 ''')
 
 conn.commit()
