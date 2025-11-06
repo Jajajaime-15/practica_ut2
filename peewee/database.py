0ab7be9c30f53e5d0553f6be2bcaf5fd8fad01c5
DAM2B-24 <jaime.ruiz8@educa.madrid.org>
2025-11-06 17:31:39 +0100

    database

--- peewee/database.py
+++ peewee/database.py
@@ -1,1 +1,17 @@
 from peewee import * # type: ignore
+import logging
+from pathlib import Path
+
+logger = logging.getLogger('peewee')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.INFO)
+
+BASE_DIR = Path(__file__).resolve().parent
+DB_PATH = BASE_DIR / "libros.db"
+
+if not DB_PATH.exists():
+    logger.error(f"La base de datos {DB_PATH} no existe, se creará al conectar")
+else:
+    logger.info(f"Conectando con la base de datos {DB_PATH}")
+
+db = SqliteDatabase(DB_PATH)
