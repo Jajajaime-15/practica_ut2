3f14ae65604e927176a4efbea09d1fb1053ac432
Jaime Ruiz <jajajaime1503@gmail.com>
2025-11-11 00:17:10 +0100

    fin repaso

--- peewee/main.py
+++ peewee/main.py
@@ -24,23 +24,25 @@ def actualizar():
     GeneroRepository.consulta_todos()
     AutorRepository.actualizar_autor("jaime ruiz","JAIME RUIZ")
     AutorRepository.consulta_todos()
-    LibrosRepository.actualizar_libros("juramentada","juramentada2")
+    LibrosRepository.actualizar_libro("juramentada","juramentada2")
     LibrosRepository.consulta_todos()
 
 def eliminar():
-    pass
+    # Eliminar
+    EditorialRepository.eliminar_editorial("nova2")
+    EditorialRepository.consulta_todos()
+    GeneroRepository.eliminar_genero("fantasia2")
+    GeneroRepository.consulta_todos()
+    AutorRepository.eliminar_autor("JAIME RUIZ")
+    AutorRepository.consulta_todos()
+    LibrosRepository.eliminar_libro("juramentada2")
+    LibrosRepository.consulta_todos()
 
 def main():
-    # Eliminar
-    EditorialRepository.eliminar_editorial("nova")
-    EditorialRepository.consulta_todos()
-    GeneroRepository.eliminar_genero("fantasia")
-    GeneroRepository.consulta_todos()
-    AutorRepository.eliminar_autor("jaime ruiz")
-    AutorRepository.consulta_todos()
-    LibrosRepository.consulta_todos()
-    LibrosRepository.eliminar_libro("juramentada")
-    LibrosRepository.consulta_todos()
+    #db.execute_sql("PRAGMA foreign_keys = ON;") #funciona con esto tmb pero cuidado porque a veces va raro
+    mostrar()
+    actualizar()
+    eliminar()
 
 
 if __name__ == '__main__':
